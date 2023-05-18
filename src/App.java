d3e0caadf445e677a499a40e926852757010bf84
Silvia <Silviaillesca402@gmail.com>
2023-05-17 22:28:09 -0600

    set new variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    int resp= 0;
+    Scanner sc = new Scanner(System.in);
+
+    ArrayList<String> cities = new ArrayList<String>();
+
+    Graph graphNormal;
+    Graph graphRain;
+    Graph graphSnow;
+    Graph graphStorm;
+    
+    List<Edge> edgesNormal = new ArrayList<>();
+    List<Edge> edgesRain = new ArrayList<>();
+    List<Edge> edgesSnow = new ArrayList<>();
+    List<Edge> edgesStorm = new ArrayList<>();
+
+
+    int weather = 1;
+
 }
