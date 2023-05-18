1711718733addecbbe68da347ab7db29966d7885
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-05-17 22:20:24 -0600

    floyd algorithm

--- src/Graph.java
+++ src/Graph.java
@@ -75,4 +75,79 @@ public class Graph {
     }
 
 
+    public  int[][] floydWarshall() {
+        int V = adjlist.size();
+        int[][] dist = new int[V][V];
+
+        // Inicializa la matriz de distancias con los valores del grafo original
+        for (int i = 0; i < V; i++) {
+            Arrays.fill(dist[i], INF);
+            for (Node neighbor : adjlist.get(i)) {
+                dist[i][neighbor.value] = neighbor.weight;
+            }
+            dist[i][i] = 0;
+        }
+
+        // Itera por cada vértice como un posible vértice intermedio
+        for (int k = 0; k < V; k++) {
+            // Itera por cada par de vértices como origen y destino
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    // Si el vértice k mejora la distancia de i a j, actualiza la distancia
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        // Imprime la matriz de distancias resultante
+        printDistances(dist);
+        return dist;
+    }
+
+    public static void printDistances(int[][] dist) {
+        int V = dist.length;
+        System.out.println("Matriz de distancias resultante:");
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF) {
+                    System.out.print("INF ");
+                } else {
+                    System.out.print(dist[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public List<Integer> getCenter(){
+        int[][] dist = floydWarshall();
+        int n = adjlist.size();
+        int[] eccentricity = new int[n];
+        for (int i = 0; i < n; i++) {
+            eccentricity[i] = 0;
+            for (int j = 0; j < n; j++) {
+                eccentricity[i] = Math.max(eccentricity[i], dist[i][j]);
+            }
+        }
+
+        int minEccentricity = INF;
+        for (int i = 0; i < n; i++) {
+            minEccentricity = Math.min(minEccentricity, eccentricity[i]);
+        }
+
+        // Encuentra los vértices con excentricidad igual a la mínima
+        List<Integer> center = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (eccentricity[i] == minEccentricity) {
+                center.add(i);
+            }
+        }
+
+        System.out.println("Centro del grafo: " + center);
+        return center;
+    }
+
+
 }
