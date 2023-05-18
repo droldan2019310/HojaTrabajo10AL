498ff6fc7ac075720e1bc4574f2c8cf6d0114746
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-05-17 22:17:55 -0600

    new class node

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,21 @@
+import java.util.*;  
+//the class stores the edges of the graph  
+
+//a class to store adjacency list nodes  
+class Node   {  
+    public int value, weight;  
+    //creating a constructor of the class Vertex  
+    Node(int value, int weight){  
+        this.value = value;  
+        this.weight = weight;  
+    }  
+    //overrides the toString() method  
+    @Override  
+    public String toString(){  
+        return this.value + " (" + this.weight + ")";  
+    }  
+}  
+
 public class Graph {
     
 }
