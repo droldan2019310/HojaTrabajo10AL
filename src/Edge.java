e21ef04818ad2d24296b9b2e18858e8926b487e3
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-05-17 22:16:44 -0600

    new class edge

--- src/Edge.java
+++ src/Edge.java
@@ -1,3 +1,10 @@
-public class Edge {
-    
-}
+public class Edge{  
+    int s, d, w;  
+    //creating a constructor of the class Edge  
+    Edge(int src, int dest, int weight)   
+    {  
+        this.s = src;  
+        this.d = dest;  
+        this.w = weight;  
+    }  
+}  
